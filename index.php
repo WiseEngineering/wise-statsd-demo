69a62bcb5acf4e9b05a8153330ef3a69a72c12b7
Yaroslav Martsynyuk <madnetuser@gmail.com>
2016-12-01 12:00:19 +0200

    Add form

--- index.php
+++ index.php
@@ -4,5 +4,11 @@
 
 <h1>Hello Wise Guys!</h1>
 
+<h2>What Is Your Name?</h2>
+<form action="/">
+	<input type="test" name="Name" label="Name" />
+	<input type="submit" value="Submit" />
+</form>
+
 </body>
 </html>
